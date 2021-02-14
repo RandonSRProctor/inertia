23b1bee7fa0ed2351666585c2f0aad178ee7761e
RandonSRProctor <randyproctor@gmail.com>
2021-02-14 00:40:54 -0500

    Essentially done with 1.0

--- app.js
+++ app.js
@@ -1,142 +1,154 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-    let rateOfTime = 500
+    let speedOfTime = 50
     let presentTime = 0
     let stopTime = false
+    let squares = Array.from(document.querySelectorAll('.poolContainer div'))
+    let timeInterval = setInterval(moveTimeForward, speedOfTime)
 
-    let squares = Array.from(document.querySelectorAll('.container div'))
-
-    squares.forEach( square => square.isDone = false)
-
-    let timeInterval = setInterval(moveTimeForward, rateOfTime)
-
-
- 
+    assignEventListenersToSquares()
+    assignEventListenerToTimeToggleButton()
+    assignEventListenerToSpeedButton()
+    assignEventListenerToResetButton()
 
     function moveTimeForward() {
 
+        announceBeginningOfCycle()
         
-        console.log(`Present time is now: ${presentTime}`)
+        for (let index = 0 ; index < squares.length ; index++) {
 
-        for (let i = 0 ; i < squares.length ; i++) {
+            const currentSquare = squares[index]
+            const squareToTheLeft = squares[index-1]
+            const squareToTheRight = squares[index+1]
 
-            //BELOW IS A TOOL FOR DEBUGGING lastUpdated VALUES
-            //console.log(`Index ${i} has a lastUpdated value of: ${squares[i].lastUpdated}`)
-           
-            const currentSquare = squares[i]
-            const squareToTheLeft = squares[i-1]
-            const squareToTheRight = squares[i+1]
-
+            const leftEdgeCase = 0
+            const rightEdgeCase = squares.length - 1
 
             
-            if (currentSquare.classList.contains('outMomentum') && currentSquare.isDone ===false) {
+            if (currentSquare.momentum === 'outward' && 
+                currentSquare.isDone   ===  false) {
 
-                //EDGE CASE LEFTMOST INDEX 
-                if (i === 0 && squares[i].isDone === false) {
+                if (index === leftEdgeCase && currentSquare.isDone === false) {
                     pushRight(squareToTheRight)
                     settleSquare(currentSquare)
                 }
-                //EDGE CASE RIGHTMOST INDEX
-                else if (i === squares.length - 1 ) {
+                else if (index === rightEdgeCase && currentSquare.isDone === false ) {
                     pushLeft(squareToTheLeft)
                     settleSquare(currentSquare)
                 }
-                //STANDARD OUT MOMENTUM
                 else {
                     pushLeft(squareToTheLeft)
                     pushRight(squareToTheRight)
                     settleSquare(currentSquare)
                 }
-                
             }
 
-            //LEFT MOMENTUM
-            else if (squares[i].classList.contains('leftMomentum') && 
-                     i != 0 &&
-                     squares[i].isDone ===false) {
+            else if (currentSquare.momentum === 'left' &&
+                     currentSquare.isDone === false &&
+                     index != leftEdgeCase) {
 
                 pushLeft(squareToTheLeft)
                 settleSquare(currentSquare)
             }
 
-            //RIGHT MOMENTUM
-            else if (currentSquare.classList.contains('rightMomentum') &&
-                    currentSquare.lastUpdated != presentTime &&
-                     i != squares.length - 1 &&
-                     currentSquare.isDone === false) {
+            else if (currentSquare.momentum === 'right' &&
+                     currentSquare.lastUpdated != presentTime &&
+                     currentSquare.isDone === false &&
+                     index != rightEdgeCase) {
 
                 pushRight(squareToTheRight)
                 settleSquare(currentSquare)
-
-
             }
         }
 
-        if (stopTime === true) {
-            clearInterval(timeInterval)
-        }
-
+        if (stopTime === true) clearInterval(timeInterval)
         
-
-            console.log('Time moves ever forward...')
-            presentTime++
-            console.log(`Present time is now ${presentTime}`)
-        
-        
-
+        incrementTime()
         
     }
 
-    
-
-
-
-    for (let i = 0 ; i < squares.length ; i++) 
-    {
-        squares[i].addEventListener('click', (event) => 
-                                        {
-                                            const square = event.target
-                                            pushOut(square)
-                                        }
-                                    )
-
+    function assignEventListenersToSquares() {
+        for (let i = 0 ; i < squares.length ; i++) {
+            squares[i].addEventListener('click', (event) => {
+                const square = event.target
+                pushOut(square)
+            })
+        }
     }
 
-    let timeToggleButton = document.querySelector('.timeToggle')
+    function assignEventListenerToTimeToggleButton() {
 
-    timeToggleButton.addEventListener('click', () => {
-        if (stopTime === false) {
-            stopTime = true
-            console.log('You have stopped time.  Are you a wizard?')
-        } else {
-            stopTime = false
-            timeInterval = setInterval(moveTimeForward, rateOfTime)
-            console.log('Time has started again!')
-        }
-    })
+        let timeToggleButton = document.querySelector('.timeToggle')
+
+        timeToggleButton.addEventListener('click', () => {
+            if (stopTime === false) {
+                stopTime = true
+                timeToggleButton.innerHTML = 'Unfreeze Time'
+                console.log('You have stopped time.  Are you a wizard?')
+            } else {
+                stopTime = false
+                timeInterval = setInterval(moveTimeForward, speedOfTime)
+                timeToggleButton.innerHTML = 'Freeze Time'
+                console.log('Time has started again!')
+            }
+        })
+    }
+
+    function assignEventListenerToSpeedButton() {
+
+        let speedOfTimeButton = document.querySelector('.speedOfTimeToggle')
+
+        speedOfTimeButton.addEventListener('click', () => {
+            if (speedOfTime === 50) {
+                speedOfTime = 500
+                clearInterval(timeInterval)
+                timeInterval = setInterval(moveTimeForward, speedOfTime)
+                speedOfTimeButton.innerHTML = 'Speed Up Time'
+            } else if (speedOfTime === 500) {
+                speedOfTime = 50
+                clearInterval(timeInterval)
+                timeInterval = setInterval(moveTimeForward, speedOfTime)
+                speedOfTimeButton.innerHTML = 'Slow Time'
+            }
+        })
+    }
+
+    function assignEventListenerToResetButton() {
+        
+        let resetButton = document.querySelector('.resetButton')
+
+        resetButton.addEventListener('click', () => {
+            squares.forEach( square => resetSquare(square))
+        })
+    }
 
     function pushRight(squareToBePushed) {
         squareToBePushed.classList.remove('leftMomentum')
-        squareToBePushed.classList.remove('outMomentum')
+        squareToBePushed.classList.remove('outwardMomentum')
         squareToBePushed.classList.add('rightMomentum')
+        squareToBePushed.momentum = 'right'
         squareToBePushed.lastUpdated = presentTime
+        squareToBePushed.classList.add('hasEnergy')
         squareToBePushed.isDone = false
     }
     
     function pushLeft(squareToBePushed) {
         squareToBePushed.classList.remove('rightMomentum')
-        squareToBePushed.classList.remove('outMomentum')
+        squareToBePushed.classList.remove('outwardMomentum')
         squareToBePushed.classList.add('leftMomentum')
+        squareToBePushed.momentum = 'left'
         squareToBePushed.lastUpdated = presentTime
+        squareToBePushed.classList.add('hasEnergy')
         squareToBePushed.isDone = false
     }
 
     function pushOut(squareToBePushed) {
         squareToBePushed.classList.remove('leftMomentum')
         squareToBePushed.classList.remove('rightMomentum')
-        squareToBePushed.classList.add('outMomentum')
-        squareToBePushed.hasOutMomentum = true
+        squareToBePushed.classList.add('outwardMomentum')
+        squareToBePushed.momentum = 'outward'
         squareToBePushed.lastUpdated = presentTime
+        squareToBePushed.classList.add('hasEnergy')
         squareToBePushed.isDone = false
 
     }
@@ -144,6 +156,25 @@ document.addEventListener('DOMContentLoaded', () => {
     function settleSquare(squareToBeSettled) {
         squareToBeSettled.lastUpdated = presentTime
         squareToBeSettled.isDone = true
+        squareToBeSettled.classList.remove('hasEnergy')
+        squareToBeSettled.momentum = 'none'
+    }
+
+    function resetSquare(squareToBeReset) {
+        squareToBeReset.classList.remove('leftMomentum')
+        squareToBeReset.classList.remove('rightMomentum')
+        squareToBeReset.classList.remove('outwardMomentum')
+        settleSquare(squareToBeReset)
+    }
+
+    function incrementTime() {
+        console.log('The current moment now ends.  Time continues on...')
+        presentTime++
+        console.log(`Present time is changed to: ${presentTime}`)
+    }
+
+    function announceBeginningOfCycle() {
+        console.log(`Present time is now: ${presentTime}`)
     }
 
 }) //THE BOTTOM OF THE BUCKET
@@ -155,19 +186,11 @@ document.addEventListener('DOMContentLoaded', () => {
 
 Up next:
 
-When a button is clicked it should receive "out momentum", which means that any square adjacent to it will be changed
-in the next interval.  When the button is changed, it will receive "left momentum" or "right momentum" relative to the direction
-which it was changed.
 
-This should continue the process all the way to the end.
-
-I should set goals for this first version
+I should set goals for this first version.  It's essentially done, but I have 2.0, 3.0, and 4.0 to go.
 
 Also: MAKE A README.MD!!!!
 
 PROCESS EATERS TO BE AWARE OF:
-
-Each time you click a box it receives a timestamp as a class.  More clicks mean more stamps.  Once a moment has passed the box
-does not need the number any more.  Might need some garbage collection?
 
 */
