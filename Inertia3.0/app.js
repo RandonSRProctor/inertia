e6729dbe84a20741a5703c2769942efe3c47aa3b
RandonSRProctor <randyproctor@gmail.com>
2021-02-28 13:56:24 -0500

    Version 3.0 Complete

--- Inertia3.0/app.js
+++ Inertia3.0/app.js
@@ -263,10 +263,40 @@ Up next:
 -Add Readme.MD
 
 Versions:
-1.0 : MVP Simply create a system in which a touch creates a 1-dimensional ripple effect
-2.0 : Add Decay for ripples so that they fade away and become "calm" after a certain time has passed
-3.0 : Add Rebounding from outside of pool
-4.0 : Add collision dynamics so that two ripples can pass through each other rather than one defeating
-5.0 : Create y-axis with all abilities of x-axis
-6.0 : Fill in diagonal space in between x and y axis in diamond formation
+
+1.0 : COMPLETE
+-MVP Simply create a system in which a touch creates a 1-dimensional ripple effect
+
+2.0 : COMPLETE AND PUBLISHED
+-Add Decay for ripples so that they fade away and become "calm" after a certain time has passed
+
+3.0 : COMPLETE
+-Add Rebounding from outside of pool
+-Add basic ripple collision transference 
+
+4.0 : IN DEVELOPMENT
+-Give Waves a lifetime rather than continuing infinitely
+
+5.0 : 
+-Redesign pool to have Y-Axis
+-Create Y-axis ripple rules based on X-axis
+
+6.0 : 
+-Fill in diagonal space in between x and y axis
+
+
+POSSIBLE OPTIMIZATIONS:
+
+-Create array model for degree of separation from DOM (Moving from Imperative to Declarative)
+
+-Change names to be more descriptive and accurate to their actions
+
+
+FUN FEATURES TO ADD:
+
+-Alternative colors schemes (psychadelic would look cool!)
+
+-Infinite Wave Toggle
+
+-Distinguish "Reset" button from "Stop Propogation"
 */
